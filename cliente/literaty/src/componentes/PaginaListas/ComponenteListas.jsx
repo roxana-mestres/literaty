5da39eac41bace7d884df9b56123d4c42f7e62fe
Roxana <123836346+roxana-mestres@users.noreply.github.com>
2024-07-04 11:42:37 +0200

    página listas: crear icono nueva lista

--- cliente/literaty/src/componentes/PaginaListas/ComponenteListas.jsx
+++ cliente/literaty/src/componentes/PaginaListas/ComponenteListas.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import estilos from "../../estilos/Comunes.module.css";
 import resena from "../../estilos/PaginaResena.module.css";
@@ -6,65 +7,86 @@ import principal from "../../estilos/PaginaPrincipal.module.css";
 import ItemLibro from "../PaginaPerfil/ComponenteItemLibro";
 import BotonSubir from "../../componentes/PaginaPerfil/ComponenteBotonSubir";
 
+const iconosLibros = [
+  "/src/assets/libro-1.svg",
+  "/src/assets/libro-2.svg",
+  "/src/assets/libro-3.svg",
+  "/src/assets/libro-4.svg",
+  "/src/assets/libro-5.svg",
+  "/src/assets/libro-6.svg",
+];
 
 function ComponenteListas() {
-  const libros = [
+  const [listasDeLibros, setListasDeLibros] = useState([
+    {
+      nombre: "Me gustan",
+      icono: iconosLibros[0],
+      editable: false,
+    },
+  ]);
+  const [libros, setLibros] = useState([
     {
       titulo: "Crimen y Castigo",
       autor: "Fiodor Dostoievski",
       genero: "Ficción literaria",
       puntuacion: "⭐⭐⭐⭐⭐",
     },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    {
-      titulo: "Crimen y Castigo",
-      autor: "Fiodor Dostoievski",
-      genero: "Ficción literaria",
-      puntuacion: "⭐⭐⭐⭐⭐",
-    },
-    // ... más libros
-  ];
+    // más libros
+  ]);
+  const [indiceSeleccionado, setIndiceSeleccionado] = useState(null);
+
+  const agregarLista = () => {
+    const nuevoIndice = listasDeLibros.length % iconosLibros.length;
+    const nuevaLista = {
+      nombre: `Nueva Lista ${listasDeLibros.length + 1}`,
+      icono: iconosLibros[nuevoIndice],
+      editable: true,
+    };
+    setListasDeLibros([...listasDeLibros, nuevaLista]);
+  };
+
+  const eliminarLista = (index) => {
+    if (window.confirm("¿Estás seguro de que deseas eliminar esta lista?")) {
+      const nuevasListas = listasDeLibros.filter((_, i) => i !== index);
+      setListasDeLibros(nuevasListas);
+      setIndiceSeleccionado(null);
+    }
+  };
+
+  const manejarEditarNombre = (e, index) => {
+    const nuevoNombre = e.target.value;
+    const nuevasListas = listasDeLibros.map((lista, i) => {
+      if (i === index) {
+        return { ...lista, nombre: nuevoNombre };
+      }
+      return lista;
+    });
+    setListasDeLibros(nuevasListas);
+  };
+
+  const manejarClickLista = (index) => {
+    setIndiceSeleccionado(index);
+  };
+
+  const manejarBlurNombre = (index) => {
+    const nuevasListas = listasDeLibros.map((lista, i) => {
+      if (i === index) {
+        return { ...lista, editable: false };
+      }
+      return lista;
+    });
+    setListasDeLibros(nuevasListas);
+  };
+
+  const manejarEditarClick = (index) => {
+    const nuevasListas = listasDeLibros.map((lista, i) => {
+      if (i === index) {
+        return { ...lista, editable: true };
+      }
+      return lista;
+    });
+    setListasDeLibros(nuevasListas);
+  };
 
   return (
     <>
@@ -81,44 +103,86 @@ function ComponenteListas() {
           <h1 className={listas["titulo"]}>Tus listas</h1>
         </div>
         <div className={listas["div-iconos-mas"]}>
-          <div className={listas["circulo-contenedor"]}>
+          <div className={listas["circulo-contenedor"]} onClick={agregarLista}>
             <div className={listas["circulo"]}>
               <span className={listas["mas"]}>+</span>
             </div>
           </div>
           <div className={listas["iconos-lista"]}>
-            <div className={listas["titulo-lista"]}>
-              <img
-                src="/src/assets/libro-1.svg"
-                alt="icono libro"
-                style={{ width: "100px", marginLeft: "20px" }}
-              ></img>
-              <p className={listas["parrafos"]}>Me gustan</p>
-            </div>
-            <div className={listas["titulo-lista"]}>
-              <img
-                src="/src/assets/libro-4.svg"
-                alt="icono libro"
-                style={{ width: "100px", marginLeft: "20px" }}
-              ></img>
-              <p className={listas["parrafos"]}>Libros por comprar</p>
-            </div>
-            <div className={listas["titulo-lista"]}>
-              <img
-                src="/src/assets/libro-2.svg"
-                alt="icono libro"
-                style={{ width: "100px", marginLeft: "20px" }}
-              ></img>
-              <p className={listas["parrafos"]}>Libros por releer</p>
-            </div>
-            <div className={listas["titulo-lista"]}>
-              <img
-                src="/src/assets/libro-3.svg"
-                alt="icono libro"
-                style={{ width: "100px", marginLeft: "20px" }}
-              ></img>
-              <p className={listas["parrafos"]}>Libros por leer</p>
-            </div>
+            {listasDeLibros.map((lista, index) => (
+              <div
+                key={index}
+                className={listas["titulo-lista"]}
+                onClick={() => manejarClickLista(index)}
+              >
+                <img
+                  src={lista.icono}
+                  alt="icono libro"
+                  style={{ width: "100px", marginLeft: "20px" }}
+                />
+                {indiceSeleccionado === index && (
+                  <span
+                    className="material-symbols-outlined"
+                    style={{
+                      position: "absolute",
+                      cursor: "pointer",
+                      fontSize: "24px",
+                      right: "20px",
+                      top: "-50px",
+                      color: "#22222",
+                    }}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      eliminarLista(index);
+                    }}
+                  >
+                    delete
+                  </span>
+                )}
+                {indiceSeleccionado === index && (
+                  <span
+                    className="material-symbols-outlined"
+                    style={{
+                      position: "absolute",
+                      cursor: "pointer",
+                      fontSize: "24px",
+                      right: "50px",
+                      top: "-50px",
+                      color: "#22222",
+                    }}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      manejarEditarClick(index);
+                    }}
+                  >
+                    edit
+                  </span>
+                )}
+                {lista.editable ? (
+                  <input
+                    type="text"
+                    value={lista.nombre}
+                    onChange={(e) => manejarEditarNombre(e, index)}
+                    onBlur={() => manejarBlurNombre(index)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        manejarBlurNombre(index);
+                      }
+                    }}
+                    autoFocus
+                    style={{
+                      color: "#000000",
+                      backgroundColor: "transparent",
+                      border: "none",
+                      fontSize: "16px",
+                      width:"150px"
+                    }}
+                  />
+                ) : (
+                  <p className={listas["parrafos"]}>{lista.nombre}</p>
+                )}
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -126,7 +190,7 @@ function ComponenteListas() {
         <h1 className={listas["titulo-seccion-libros"]}>
           Libros que me gustan
         </h1>
-        <div className={`${principal["seccion-libros-grid"]}`}>
+        <div className={principal["seccion-libros-grid"]}>
           {libros.map((libro, index) => (
             <div key={index} className={principal["item-libro"]}>
               <ItemLibro
